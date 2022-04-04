46dc9f43fcb085426c5f1b2039705c4cc2bbda53
sunidhi07 <59998582+sunidhi07@users.noreply.github.com>
2022-04-04 13:33:21 +0530

    Add files via upload

---
+++ decoder1/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,2 @@
+1631699397 /home/adld11/cds.lib
+1638443235 /home/adld11/vardhan/class4/decoder_tb.v
---
+++ decoder1/genus.cmd
@@ -0,0 +1,10 @@
+# Cadence Genus(TM) Synthesis Solution, Version 17.22-s017_1, built Apr  1 2018
+
+# Date: Thu Dec 02 16:21:27 2021
+# Host: VLSI43 (x86_64 w/Linux 2.6.32-754.el6.x86_64) (4cores*8cpus*1physical cpu*Intel(R) Core(TM) i7-6700 CPU @ 3.40GHz 8192KB)
+# OS:   Red Hat Enterprise Linux Client release 6.10 (Santiago)
+
+pwd
+source synthesis.tcl
+nclaunch -new
+exit
